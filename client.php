a61800abf62d6f817acc18e7c6e5e3cfc3264d8e
Bechir <bechir@accessnow.org>
2014-01-26 03:19:44 +0100

    Adding protocol for user request (overide http to https when possible)

--- client.php
+++ client.php
@@ -8,7 +8,7 @@
     mysql_select_db($config->{'database'})  or die("erreur") ;
 
     //getting data from the database
-    $request = "SELECT D1.domain AS 'target', D2.domain AS 'fake' , D2.has_malware AS 'malware', D2.protocol AS 'protocol' FROM domain AS D1 LEFT JOIN domain AS D2 ON D1.domain = D2.target WHERE D2.is_fake =1 ;" ;
+    $request = "SELECT D1.domain AS 'target', D2.domain AS 'fake' , D2.has_malware AS 'malware', D1.protocol AS 'fake protocol', D2.protocol AS 'target protocol' FROM domain AS D1 LEFT JOIN domain AS D2 ON D1.domain = D2.target WHERE D2.is_fake =1 ;" ;
     $result = mysql_query($request) ;
     
 
@@ -20,7 +20,7 @@
     while( $data = mysql_fetch_array($result) )
     {
         $i++;
-        $jsonData .= ' "f'.$i.'":{ "domain":"'.$data['fake'].'", "origin":"'.$data['target'].'", "malware":"'.$data['malware'].'", "protocol":"'.$data['protocol'].'" } ' ;
+        $jsonData .= ' "f'.$i.'":{ "domain":"'.$data['fake'].'", "origin":"'.$data['target'].'", "malware":"'.$data['malware'].'", "target_protocol":"'.$data['target protocol'].'", "fake_protocol":"'.$data['fake protocol'].'" } ' ;
         if ($i != $n ) 
         {
           $jsonData .= ',
